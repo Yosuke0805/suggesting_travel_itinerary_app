3dabd705a210036a3e0fd52f0f9d3d60375fd122
Yosuke Kawazoe <ysk0805.bv@gmail.com>
2024-10-06 13:16:05 +0100

    implemented login

--- suggest_travel_itinerary_app.py
+++ suggest_travel_itinerary_app.py
@@ -19,7 +19,6 @@ from google.api_core.exceptions import ResourceExhausted
 
 # Config
 model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
-GEMINI_API_KEY = None
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -50,18 +49,16 @@ def main():
                     # for Streamlit Community Cloud : load API key using Streamlit secrets
                     if not GEMINI_API_KEY:
                         GEMINI_API_KEY = st.secrets["api_keys"]["GEMINI_API_KEY"]
+                    # configure model with api key
+                    genai.configure(api_key=GEMINI_API_KEY)
                 else:
                     st.sidebar.error("Invalid password")
         elif user_type == "Others":
             # set Gemini API
             GEMINI_API_KEY = st.sidebar.text_input("Input your Gemini API key", type="password")
-
-        # configure model with api key
-        if GEMINI_API_KEY:
-            # Use GEMINI_API_KEY in your application logic
+            # configure model with api key
             genai.configure(api_key=GEMINI_API_KEY)
-        else:
-            st.error("API key is missing. Please provide a valid Gemini API key.")
+
 
         # Input fields
         traveling_days = st.number_input("Number of traveling days", min_value=1, max_value=99, value=3)
